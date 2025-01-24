844614c69bd8f46246819c4400acc1b40467fbc5
wiamerz <wiamerm2023@gmail.com>
2025-01-24 12:09:05 +0100

    main carte, search bar, weather icons

---
+++ script.js
@@ -0,0 +1,57 @@
+
+const apikey = "a32aedf513bbc72a70a0e671b422dc02";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
+
+
+
+
+
+async function checkWeather(city){
+  const response = await fetch(apiurl + city +`&appid=${apikey}`);
+
+  if(response.status == 404){
+    document.querySelector(".error").style.display="block";
+    document.querySelector(".weather").style.display="none";
+  }else{
+    var data = await response.json();
+
+    // console.log(data);
+  
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity+ "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+  
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+  
+    }
+    else if (data.weather[0].main == "Clear"){
+      weatherIcon.src = "images/cs.png";
+    }
+    else if (data.weather[0].main == "Rain"){
+      weatherIcon.src = "images/rain.png";
+    }
+    else if (data.weather[0].main == "Mist"){
+      weatherIcon.src = "images/fog.png";
+    }
+    else if (data.weather[0].main == "Snowy"){
+      weatherIcon.src = "images/snow.png";
+    }
+  
+    document.querySelector(".weather").style.display ="block";
+    document.querySelector(".error").style.display="none";
+  
+  }
+  
+}
+
+searchBtn.addEventListener("click",()=> {
+checkWeather(searchBox.value);
+})
+
+
